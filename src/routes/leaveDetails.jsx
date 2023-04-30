782b9fcaff4d3da4f3ce04b28fb0070230164c88
relish <guptarelish333@gmail.com>
2023-05-01 03:29:54 +0530

    updated tailwind.config

--- src/routes/leaveDetails.jsx
+++ src/routes/leaveDetails.jsx
@@ -1,11 +1,17 @@
+import { UserContext } from "../contexts/emp.context";
+import { useContext } from "react";
+
 const LeaveDetails = () => {
+
+    const {emp}=useContext(UserContext);
+
     return (
       <section className='relative font-serif bg-gradient-to-r from-green-400 to-blue-500 min-h-screen'>
           <div className=" flex flex-col justify-between">
               <div className=" flex flex-row pt-5 md:pt-32 md:ml-20 lg:mx-44 xl:ml-72 sz:ml-96 text-2xl">
                   <i className="fa-solid fa-user fa-beat"/>
                   <div className="ml-1">
-                    <h1 className="md:border-b">Name of the employee</h1>
+                    <h1 className="md:border-b">{emp}</h1>
                   </div>
               </div>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        '30':'51rem',
+      },
+      screens:{
+        'xl':'1281px',
+        'sz':'1900px',
+      },
+    },
   },
   plugins: [],
 }
